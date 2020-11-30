3482f5939cad1daf9e3d1aec1dce73c7a36ecc2d
Albert Peschar <albert@peschar.net>
2020-11-30 18:30:41 +0000

    Use <?= ... ?> in view files

--- common/view/adminhtml/layout/default.xml.php
+++ common/view/adminhtml/layout/default.xml.php
@@ -1,6 +1,6 @@
-<?php echo '<?xml version="1.0"?>';?>
+<?= '<?xml version="1.0"?>'; ?>
 <page xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" layout="admin-1column" xsi:noNamespaceSchemaLocation="urn:magento:framework:View/Layout/etc/page_configuration.xsd">
    <head>
-        <css src="<?php echo $argv[1] ;?>_Magento2::css/styles.css" />
+        <css src="<?= $argv[1]; ?>_Magento2::css/styles.css" />
    </head>
 </page>

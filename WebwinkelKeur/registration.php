8cfb3b1699834ca99b188476cd7f60c0c68fc8ee
tsanko <tsankohadzhiev@gmail.com>
2020-12-07 15:29:59 +0200

    Add prefix for Import actino

--- WebwinkelKeur/registration.php
+++ WebwinkelKeur/registration.php
@@ -1,4 +1,6 @@
 <?php
+namespace WebwinkelKeur\Magento2;
+
 use Magento\Framework\Component\ComponentRegistrar;
 
 ComponentRegistrar::register(
@@ -7,3 +9,15 @@ ComponentRegistrar::register(
     isset($file) && realpath($file) == __FILE__ && getenv('VALUED_DEVELOPMENT') == 'yes'
         ? dirname($file) : __DIR__
 );
+
+spl_autoload_register(function ($cls) {
+    $prefix = __NAMESPACE__ . '\\Controller\\Adminhtml\\';
+    if (strpos($cls, $prefix) !== 0) {
+        return;
+    }
+    $base_class = 'Valued\\Magento2\\Controller\\Adminhtml\\Actions\\Import';
+    if (!class_exists($base_class)) {
+        return;
+    }
+    class_alias($base_class, $cls);
+});

387cf6540d1597bdca927149faf49c039df88be6
Albert Peschar <albert@peschar.net>
2020-12-08 09:48:45 +0000

    Remove spl_autoload_register hack

--- WebwinkelKeur/registration.php
+++ WebwinkelKeur/registration.php
@@ -9,15 +9,3 @@ ComponentRegistrar::register(
     isset($file) && realpath($file) == __FILE__ && getenv('VALUED_DEVELOPMENT') == 'yes'
         ? dirname($file) : __DIR__
 );
-
-spl_autoload_register(function ($cls) {
-    $prefix = __NAMESPACE__ . '\\Controller\\Adminhtml\\';
-    if (strpos($cls, $prefix) !== 0) {
-        return;
-    }
-    $base_class = 'Valued\\Magento2\\Controller\\Adminhtml\\Actions\\Import';
-    if (!class_exists($base_class)) {
-        return;
-    }
-    class_alias($base_class, $cls);
-});

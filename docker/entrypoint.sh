21e537674917fc1048678a07c2b2fd4f419d4a83
Albert Peschar <albert@peschar.net>
2022-05-17 12:52:14 +0000

    Don't sudo before stat (forget about weird Dockers)

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 cd /
 
-usermod -u "$(sudo -u apache stat -c %u /data/www)" apache
+usermod -u "$(stat -c %u /data/www)" apache
 
 mkdir -p /run/apache2
 exec httpd -D FOREGROUND -f /etc/apache2/httpd.conf

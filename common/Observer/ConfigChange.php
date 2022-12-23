b526953bd1c6523fdc5ba3e9942e8b2a85d2e53a
Tsanko Hadzhiev <tsankohadzhiev@gmail.com>
2022-12-23 09:57:21 +0200

    Minor improvements on ConfigChange.php

--- common/Observer/ConfigChange.php
+++ common/Observer/ConfigChange.php
@@ -9,25 +9,25 @@ use Valued\Magento2\Model\Api as ApiModel;
 use Valued\Magento2\Setup\ExtensionBase;
 
 class ConfigChange implements ObserverInterface {
-    private $apiModel;
+    private ApiModel $apiModel;
 
-    private $_urlBuilder;
+    private Url $urlBuilder;
 
-    private $extension;
+    private ExtensionBase $extension;
 
     public function __construct(
-        ApiModel        $apiModel,
-        Url             $urlBuilder,
-        ExtensionBase   $extension
+        ApiModel $apiModel,
+        Url $urlBuilder,
+        ExtensionBase $extension
     ) {
         $this->apiModel = $apiModel;
-        $this->_urlBuilder = $urlBuilder;
+        $this->urlBuilder = $urlBuilder;
         $this->extension = $extension;
     }
 
     public function execute(Observer $observer) {
         $storeId = $observer->getEvent()->getData('store') ?: null;
-        $syncUrl = $this->_urlBuilder->getUrl(sprintf('%s/sync', $this->extension->getSlug()));
+        $syncUrl = $this->urlBuilder->getUrl(sprintf('%s/sync', $this->extension->getSlug()));
         $this->apiModel->sendSyncUrl($syncUrl, $storeId);
     }
 }

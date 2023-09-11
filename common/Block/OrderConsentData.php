9b56bebafd1ea06a2dcb9ad99e07ecf54331e4ea
Tsanko Hadzhiev <tsankohadzhiev@gmail.com>
2023-09-11 15:10:41 +0300

    Add consent data script

---
+++ common/Block/OrderConsentData.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Valued\Magento2\Block;
+
+use Magento\Checkout\Model\Session;
+use Magento\Sales\Model\Order;
+use Magento\Backend\Block\Template\Context;
+use Magento\Framework\View\Element\Template;
+use Magento\Sales\Model\OrderFactory;
+use Magento\Store\Model\StoreManagerInterface;
+use Magento\Widget\Block\BlockInterface;
+use Valued\Magento2\Helper\Invitation as InvitationHelper;
+use Valued\Magento2\Setup\ExtensionBase;
+
+class OrderConsentData extends Template implements BlockInterface {
+
+    private $extension;
+    private $orderFactory;
+
+    private $checkoutSession;
+
+    private $configs;
+
+    public function __construct(
+        Context $context,
+        InvitationHelper $invitationHelper,
+        StoreManagerInterface $storeManager,
+        ExtensionBase $extension,
+        OrderFactory $orderFactory,
+        Session $checkoutSession,
+        array $data = []
+    ) {
+        $this->invitationHelper = $invitationHelper;
+        $this->storeManager = $storeManager;
+        $this->extension = $extension;
+        $this->orderFactory = $orderFactory;
+        $this->checkoutSession = $checkoutSession;
+        $this->configs = $invitationHelper->getConfigData($this->storeManager->getStore()->getId());
+        parent::__construct($context, $data);
+    }
+
+    public function getConsentData(): string {
+        $store_id = $this->storeManager->getStore()->getId();
+        $config = $this->invitationHelper->getConfigData($store_id);
+
+        if (!$config) {
+            return '';
+        }
+
+        if (!$config['webshop_id']) {
+            return '';
+        }
+
+        $order = $this->getOrder();
+        $consent_data = [
+            'webshopId' => $config['webshop_id'],
+            'orderNumber' => $order->getIncrementId(),
+            'email' => $order->getCustomerEmail(),
+            'firstName' => $order->getBillingAddress()->getFirstname(),
+            'inviteDelay' => $config['delay'],
+        ];
+
+        return json_encode($consent_data);
+    }
+
+
+    private function getOrder(): Order {
+        return $this->orderFactory->create()->loadByIncrementId(
+            $this->checkoutSession->getLastRealOrderId());
+    }
+
+    public function consentFlowEnabled(): bool {
+        return $this->configs['consent_flow'] ?? false;
+    }
+
+    public function getSystemKey(): string {
+        return $this->extension->getSlug();
+    }
+}

e401de6366faeed49947f5713f0c121fd56fca00
Tsanko Hadzhiev <tsankohadzhiev@gmail.com>
2024-07-03 13:45:05 +0300

    Nonce backward compatibility (#24)
    * WIP
    
    * Add csp_whitelist.xml
    
    * Add blank line

--- common/Block/Sidebar.php
+++ common/Block/Sidebar.php
@@ -2,7 +2,6 @@
 namespace Valued\Magento2\Block;
 
 use Magento\Backend\Block\Template\Context;
-use Magento\Csp\Helper\CspNonceProvider;
 use Magento\Framework\View\Element\Template;
 use Magento\Widget\Block\BlockInterface;
 use Valued\Magento2\Helper\General as GeneralHelper;
@@ -13,18 +12,14 @@ class Sidebar extends Template implements BlockInterface {
 
     private $extension;
 
-    private $cspNonceProvider;
-
     public function __construct(
         Context          $context,
         GeneralHelper    $generalHelper,
         ExtensionBase    $extension,
-        CspNonceProvider $cspNonceProvider,
         array            $data = []
     ) {
         $this->generalHelper = $generalHelper;
         $this->extension = $extension;
-        $this->cspNonceProvider = $cspNonceProvider;
         parent::__construct($context, $data);
     }
 
@@ -42,9 +37,5 @@ class Sidebar extends Template implements BlockInterface {
 
     public function getLanguage() {
         return $this->generalHelper->getLanguage();
-    }
-
-    public function getNonce(): string {
-        return $this->cspNonceProvider->generateNonce();
     }
 }
